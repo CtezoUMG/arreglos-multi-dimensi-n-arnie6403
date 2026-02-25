681f2ba7a25d8b14893f0ab5ca6dc0b49fa6c29c
arnie6403 <arnie6463@gmail.com>
2026-02-25 08:25:37 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,15 @@
 using System;
-string? linea1 = Console.ReadLine();
-if (string.IsNullOrEmpty(linea1)) return;
-int n = int.Parse(linea1);
-string[] entrada = Console.ReadLine()?.Split(' ', StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>();
-int[] numeros = new int[n];
+string? l1 = Console.ReadLine();
+if (string.IsNullOrEmpty(l1)) return;
+int n = int.Parse(l1);
+string[] inp = Console.ReadLine()?.Split(' ', StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>();
+int[] num = new int[n];
 for (int i = 0; i < n; i++)
 {
-    numeros[i] = int.Parse(entrada[i]);
+    num[i] = int.Parse(inp[i]);
 }
 int m = num[0];
-for (int i = 1; i < numeros.Length; i++)
-{
-    if (numeros[i] > m)
-    {
-        m = numeros[i];
-    }
+foreach(int a in num) {
+    if (a > m) m = a;
 }
 Console.WriteLine(m);
